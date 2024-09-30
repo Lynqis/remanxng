ef02f56e982c00025ab8d992bb5b5f801167e8de
Matthieu Fallourd <fallourd.matthieu@gmail.com>
2024-09-30 23:23:39 +0200

    feat: create layout pages

--- src/app/showcase/layout/app.component.ts
+++ src/app/showcase/layout/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { IconModule } from '../../components/icon/icon';
+import { Layout } from './templates/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, IconModule, Layout],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
