7d6a6fd8254276838c0660ac1cc18f963859597e
Matthieu Fallourd <fallourd.matthieu@gmail.com>
2024-10-23 22:48:48 +0200

    feat: layout component

--- src/app/showcase/layout/pages/home.component.ts
+++ src/app/showcase/layout/pages/home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from "@angular/core";
+import { IconModule } from "../../../components/icon/icon";
+import { SidebarModule } from "../../../components/sidebar/sidebar";
 
 @Component({
   template: `<h1>Home</h1>`,
+  imports: [SidebarModule],
   standalone: true
 })
 export class HomeComponent
