d18046449fccf81dadef3497d361eebe7261bd22
Matthieu Fallourd <fallourd.matthieu@gmail.com>
2024-11-18 16:42:09 +0100

    fix: remove useless imports

--- src/app/showcase/layout/app.component.ts
+++ src/app/showcase/layout/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { IconModule } from '../../components/icon/icon';
 import { Layout } from './templates/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, IconModule, Layout],
+  imports: [CommonModule, Layout],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
