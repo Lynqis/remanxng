ca7ec002cb2e8063bce8e5af8643e92114c902b3
Matthieu Fallourd <fallourd.matthieu@gmail.com>
2024-11-19 12:16:25 +0100

    feat(component): add default template to header/footer/main

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,18 +1,32 @@
 import { CommonModule } from '@angular/common';
 import {
   Component,
+  ContentChildren,
   NgModule,
+  QueryList,
 } from '@angular/core';
+import { RxTemplate } from '../../api/directives/shared';
+import { TemplateNull } from '../../api/helpers/ts-helper';
 
 @Component({
   template: `
-    <header>
-        <ng-content></ng-content>
-    </header>
+    <ng-container *ngIf="headerTemplate; else notTemplate">
+      <ng-container *ngTemplateOutlet="headerTemplate"></ng-container>
+    </ng-container>
+    <ng-template #notTemplate>
+      <header class="rx-header">
+          <ng-content></ng-content>
+      </header>
+    </ng-template>
   `,
   selector: 'rx-header',
+  styleUrls: ['./header.css']
 })
-export class Header {}
+export class Header {
+  @ContentChildren(RxTemplate) templates: QueryList<RxTemplate> | undefined;
+
+  headerTemplate: TemplateNull<any>;
+}
 
 @NgModule({
   imports: [CommonModule],
--- src/app/components/main-content/main-content.ts
+++ src/app/components/main-content/main-content.ts
@@ -1,18 +1,32 @@
 import { CommonModule } from '@angular/common';
 import {
   Component,
+  ContentChildren,
   NgModule,
+  QueryList,
 } from '@angular/core';
+import { RxTemplate } from '../../api/directives/shared';
+import { TemplateNull } from '../../api/helpers/ts-helper';
 
 @Component({
   template: `
-    <div class="main-content">
-        <ng-content></ng-content>
-    </div>
+    <ng-container *ngIf="mainTemplate; else notTemplate">
+      <ng-container *ngTemplateOutlet="mainTemplate"></ng-container>
+    </ng-container>
+    <ng-template #notTemplate>
+      <div class="rx-main-content">
+          <ng-content></ng-content>
+      </div>
+    </ng-template>
   `,
   selector: 'rx-main',
+  styleUrls: ['./main-content.css']
 })
-export class MainContent {}
+export class MainContent {
+  @ContentChildren(RxTemplate) templates: QueryList<RxTemplate> | undefined;
+
+  mainTemplate: TemplateNull<any>;
+}
 
 @NgModule({
   imports: [CommonModule],
