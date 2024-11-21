1e5c331d31fa1ac72d951324eb6890d114b1d848
Fallourd Matthieu <fallourd.matthieu@gmail.com>
2024-11-21 16:51:55 +0100

    feat: WIP - button

---
+++ src/app/components/button/button.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'rx-button',
+    template: `
+        <button>
+        </button>
+    `,
+    standalone: true,
+})
+export class RxButton {}
