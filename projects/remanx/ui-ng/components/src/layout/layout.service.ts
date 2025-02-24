fac7eb8feee5aeee799bb94d62ed2e2e163959a1
Fallourd Matthieu <fallourd.matthieu@gmail.com>
2025-02-24 14:19:05 +0100

    feat: layout service with signal

--- projects/remanx/ui-ng/components/src/layout/layout.service.ts
+++ projects/remanx/ui-ng/components/src/layout/layout.service.ts
@@ -1,51 +1,32 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { Injectable, signal } from "@angular/core";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LayoutService {
 
-  // sidebarShrink: boolean = false;
-
-  private sidebarVisibleSubject = new BehaviorSubject<boolean>(false);
-  private sidebarShrinkSubject = new BehaviorSubject<boolean>(false);
-  private isShrinkSubject = new BehaviorSubject<boolean>(false);
-
-  sidebarVisible$ = this.sidebarVisibleSubject.asObservable();
-  sidebarShrink$ = this.sidebarShrinkSubject.asObservable();
-  isShrink$ = this.isShrinkSubject.asObservable();
+  sidebarVisible = signal(false);
+  sidebarShrink = signal(false);
+  isShrink = signal(false);
 
   toggleSidebar() {
-    console.log(this.sidebarShrink)
-    if (!this.sidebarShrink) {
-      this.setSidebarVisible(!this.sidebarVisibleSubject.value);
+    console.log(this.sidebarShrink())
+    if (!this.sidebarShrink()) {
+      this.setSidebarVisible(!this.sidebarVisible());
     } else {
-      this.setIsShrink(!this.isShrinkSubject.value);
+      this.setIsShrink(!this.isShrink());
     }
   }
 
   setSidebarVisible(value: boolean) {
-    this.sidebarVisibleSubject.next(value);
+    this.sidebarVisible.set(value);
   }
 
   setSidebarShrink(value: boolean) {
-    this.sidebarShrinkSubject.next(value);
+    this.sidebarShrink.set(value);
   }
 
   setIsShrink(value: boolean) {
-    this.isShrinkSubject.next(value);
-  }
-
-  get sidebarVisible(): boolean {
-    return this.sidebarVisibleSubject.value;
-  }
-
-  get isShrink(): boolean {
-    return this.isShrinkSubject.value;
-  }
-
-  get sidebarShrink(): boolean {
-    return this.sidebarShrinkSubject.value;
+    this.isShrink.set(value);
   }
 }
