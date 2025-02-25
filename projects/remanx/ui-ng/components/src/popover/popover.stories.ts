a8d9332539425e8a06bb801f68319ca2c4d62632
Fallourd Matthieu <fallourd.matthieu@gmail.com>
2025-02-25 12:24:35 +0100

    feat: add event in dialog

--- projects/remanx/ui-ng/components/src/popover/popover.stories.ts
+++ projects/remanx/ui-ng/components/src/popover/popover.stories.ts
@@ -1,9 +1,9 @@
-import { Meta, moduleMetadata } from "@storybook/angular";
+import { Meta, moduleMetadata, StoryObj } from "@storybook/angular";
 import { RxButton } from "../button";
 import { RxPopover } from "./popover";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-export default {
+const meta: Meta<RxPopover> = {
   title: 'Surfaces/RxPopover',
   component: RxPopover,
   tags: ['autodocs'],
@@ -15,22 +15,23 @@ export default {
       ],
     }),
   ]
-} as Meta<RxPopover>;
+}
 
-export const Default = (args: RxPopover) => ({
-  component: RxPopover,
-  template: `
-    <rx-button (click)="op.toggle($event, button)" [label]="'Open a modal'"></rx-button>
-    <rx-overlaypanel #op [$height]="'200px'" [$width]="'50%'">
-      <h1>Popover</h1>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
-    </rx-overlaypanel>
-  `,
-  props: {
-    ...args
-  }
-});
+export default meta;
+type Story = StoryObj<RxPopover>;
+
+export const Default: Story = {
+  render: () => ({
+    template: `
+      <rx-button (click)="op.toggle($event, button)" [label]="'Open a modal'"></rx-button>
+      <rx-popover #op [$height]="'200px'" [$width]="'50%'">
+        <h1>Popover</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+      </rx-popover>
+    `
+  })
+};
 
 
