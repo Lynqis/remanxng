b206ca8e7f5ee6fb803ba27bc857b096e214f1c4
Fallourd Matthieu <fallourd.matthieu@gmail.com>
2025-03-19 15:45:06 +0100

    feat: toast

--- projects/remanx/ui-ng/api/src/helpers/ts-helper.ts
+++ projects/remanx/ui-ng/api/src/helpers/ts-helper.ts
@@ -4,4 +4,4 @@ export declare type Nullable<T = void> = T | null | undefined;
 export declare type TemplateNull<T = void> = TemplateRef<any> | null | T;
 export declare type VoidListener = VoidFunction | null | undefined;
 export declare type Position = 'center' | 'left' | 'top' | 'right' | 'bottom' | 'topleft' | 'topright' | 'bottomleft' | 'bottomright';
-export declare type Severity = 'success' | 'info' | 'warning' | 'danger' | 'help' | 'primary' | 'secondary' | 'contrast' | null | undefined;
+export declare type Severity = 'success' | 'info' | 'warning' | 'help' | 'primary' | 'secondary' | 'contrast' | 'error' | null | undefined;
