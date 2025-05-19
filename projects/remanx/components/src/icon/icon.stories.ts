90ced4b54a513d9f8fa09cfcc524cd4cd00a8bf3
Matthieu Fallourd <fallourd.matthieu@gmail.com>
2025-05-19 20:52:51 +0200

    docs: add icons exemples

--- projects/remanx/components/src/icon/icon.stories.ts
+++ projects/remanx/components/src/icon/icon.stories.ts
@@ -1,5 +1,8 @@
 import { applicationConfig, Meta, moduleMetadata, StoryObj } from "@storybook/angular";
 import { RxIcon } from "./icon";
+import * as DefaultIcons from '@lynqis/remanx-icons/icons.json';
+
+const icons: Record<string, string> = (DefaultIcons as any).icons;
 
 const meta: Meta<RxIcon> = {
   title: 'Misc/RxIcon',
@@ -28,7 +31,33 @@ export const SimpleExemple: Story = {
       <rx-icon [iconJson]="'info'" [thickness]="'3'" />
       <rx-icon [iconJson]="'menu'" [stroke]="'green'" />
       <rx-icon [iconJson]="'save'" [fill]="'red'" />
+      <rx-icon [iconJson]="'rx-leanqis'" [fill]="'red'" [stroke]="'#fff'" [$height]="'2rem'" [$width]="'2rem'" />
     </div>
     `
   })
+}
+
+
+
+export const AllIcons: Story = {
+  render: () => {
+    const html = Object.keys(icons)
+      .map(
+        name => `
+          <div style="display: flex; flex-direction: column; align-items: center; gap: 4px; text-align: center; justify-content: center">
+            <rx-icon [iconJson]="'${name}'" [$height]="'60px'" [$width]="'60px'" [stroke]="'#000'" />
+            <div style="font-size: 10px">${name}</div>
+          </div>
+        `
+      )
+      .join('');
+
+    return {
+      template: `
+        <div style="display: flex; flex-wrap: wrap; gap: 30px; text-align: center">
+          ${html}
+        </div>
+      `,
+    };
+  },
 }
